a09edc12f216ecaed54812918daeca118a3394b0
georgiansof <61194150+georgiansof@users.noreply.github.com>
2021-07-14 01:22:57 +0300

    Add files via upload

---
+++ Block OS/src/LTE version/blockos/sources/hardwarecomm/pci.cpp
@@ -0,0 +1,187 @@
+#include <hardwarecomm/pci.h>
+using namespace blockos::common;
+using namespace blockos::drivers;
+using namespace blockos::hardwarecomm;
+
+void printf(char*);
+void printfHex(uint8_t);
+
+PeripherialComponentInterconnectDeviceDescriptor::PeripherialComponentInterconnectDeviceDescriptor()
+{
+	
+}
+PeripherialComponentInterconnectDeviceDescriptor::~PeripherialComponentInterconnectDeviceDescriptor()
+{
+	
+}
+
+PeripherialComponentInterconnectController::PeripherialComponentInterconnectController()
+: dataPort(0xCFC),
+  commandPort(0xCF8)
+{
+}
+
+PeripherialComponentInterconnectController::~PeripherialComponentInterconnectController()
+{
+}
+
+uint32_t PeripherialComponentInterconnectController::Read(uint16_t bus, uint16_t device, uint16_t function, uint32_t registeroffset)
+{
+	uint32_t id = 
+		0x1 << 31
+		| ((bus & 0xFF) << 16)
+		| ((device & 0x1F) << 11)
+		| ((function & 0x07) << 8)
+		| (registeroffset & 0xFC);
+	commandPort.Write(id);
+	uint32_t result = dataPort.Read();
+	return result >> (8* (registeroffset % 4));
+}
+
+void PeripherialComponentInterconnectController::Write(uint16_t bus, uint16_t device, uint16_t function, uint32_t registeroffset, uint32_t value)
+{
+		uint32_t id = 
+		0x1 << 31
+		| ((bus & 0xFF) << 16)
+		| ((device & 0x1F) << 11)
+		| ((function & 0x07) << 8)
+		| (registeroffset & 0xFC);
+		commandPort.Write(id);
+		dataPort.Write(value);
+}
+
+bool PeripherialComponentInterconnectController::DeviceHasFunctions(common::uint16_t bus, common::uint16_t device)
+{return Read(bus, device, 0, 0x0E) & (1<<7);}
+
+void PeripherialComponentInterconnectController::SelectDrivers(DriverManager* driverManager, blockos::hardwarecomm::InterruptManager* interrupts)
+{
+	for(int bus = 0; bus < 8; ++bus)
+	{
+		for(int device = 0; device < 32 ; ++device)
+		{
+			int numFunctions = DeviceHasFunctions(bus, device) ? 8 : 1; 
+			for(int function = 0; function < numFunctions ; ++ function)
+			{
+				PeripherialComponentInterconnectDeviceDescriptor dev = GetDeviceDescriptor(bus, device, function);
+				
+				if(dev.vendor_id == 0x0000 || dev.vendor_id == 0xFFFF) 
+					continue;
+				
+                for(int barNum = 0; barNum < 6; ++barNum)
+                {
+                    BaseAddressRegister bar = GetBaseAddressRegister(bus, device, function, barNum);
+                    if(bar.address && (bar.type == InputOutput))
+                        dev.portBase = (uint32_t)bar.address;
+
+                    Driver* driver = GetDriver(dev, interrupts);
+                    if(driver != 0)
+                        driverManager->AddDriver(driver);
+                }
+
+				printf("PCI BUS ");
+				printfHex(bus & 0xFF);
+				
+				printf(", DEVICE ");
+				printfHex(device & 0xFF);
+				
+				printf(", FUNCTION ");
+				printfHex(function & 0xFF);
+				
+				printf(", VENDOR ");
+				printfHex((dev.vendor_id & 0xFF00) >> 8);
+				printfHex(dev.vendor_id & 0xFF);
+				
+				printf(", DEVICE_ID ");
+				printfHex((dev.device_id & 0xFF00) >> 8);
+				printfHex(dev.device_id & 0xFF);
+				printf("\n");
+			}
+		}
+	}
+}
+
+Driver* PeripherialComponentInterconnectController::GetDriver(PeripherialComponentInterconnectDeviceDescriptor dev, InterruptManager* interrupts)
+{
+    switch(dev.vendor_id)
+    {
+        case 0x1022: // AMD
+            switch(dev.device_id)
+            {
+                case 0x2000: // am79c973
+                    printf("AMD am79c973\n");
+                    break;
+            }
+            break;
+
+        case 0x8086: // intel
+            break;
+    }
+
+    switch(dev.class_id)
+    {
+        case 0x03:// graphics
+            switch(dev.subclass_id)
+            {
+                case 0x00: // VGA
+                    break;
+            }
+            break;
+    }
+
+    return 0;
+}
+
+BaseAddressRegister PeripherialComponentInterconnectController::GetBaseAddressRegister(uint16_t bus, uint16_t device, uint16_t function, uint16_t bar)
+{
+    BaseAddressRegister result;
+
+    uint32_t headertype = Read(bus, device, function, 0x0E) & 0x7F;
+    int maxBARs = 6 - (4*headertype);
+    if(bar >= maxBARs)
+        return result;
+
+    uint32_t bar_value = Read(bus, device, function, 0x10 + 4*bar);
+    result.type = (bar_value & 0x1) ? InputOutput : MemoryMapping;
+    uint32_t temp;
+
+    if(result.type == MemoryMapping)
+    {
+        switch((bar_value >> 1) & 0x3)
+        {
+            case 0: // 32 bit mode
+            case 1: // 20 bit mode
+            case 2: // 64 bit mode
+                break;
+        }
+    }
+    else // InputOutput
+    {
+        result.address = (uint8_t*)(bar_value & ~0x3);
+        result.prefetchable = false;
+    }
+
+    return result;
+}
+
+PeripherialComponentInterconnectDeviceDescriptor PeripherialComponentInterconnectController::GetDeviceDescriptor(common::uint16_t bus, common::uint16_t device, common::uint16_t function)
+{
+	PeripherialComponentInterconnectDeviceDescriptor result;
+	
+	result.bus = bus;
+	result.device = device;
+	result.function = function;
+	
+	result.vendor_id = Read(bus, device, function, 0x00);
+	result.device_id = Read(bus, device, function, 0x02);
+	
+	result.class_id = Read(bus, device, function, 0x0B);
+	result.subclass_id = Read(bus, device, function, 0x0A);
+	result.interface_id = Read(bus, device, function, 0x09);
+	
+	result.revision = Read(bus, device, function, 0x08);
+	result.interrupt = Read(bus, device, function, 0x3C);
+	
+	return result;
+}
+
+
---
+++ Block OS/src/LTE version/blockos/sources/kernel.cpp
@@ -0,0 +1,145 @@
+#include <common/types.h>
+#include <gdt.h>
+#include <hardwarecomm/interrupts.h>
+#include <hardwarecomm/pci.h>
+#include <drivers/driver.h>
+#include <drivers/keyboard.h>
+#include <drivers/mouse.h>
+
+using namespace blockos;
+using namespace blockos::common;
+using namespace blockos::hardwarecomm;
+using namespace blockos::drivers;
+
+void printf(char* str)
+{
+    static volatile uint16_t* VideoMemory = (uint16_t*)0xb8000; 
+    static uint8_t x=0, y=0;
+    
+    for(int i=0;str[i] != '\0'; ++i)
+    {
+        
+        switch(str[i])
+        {
+            case '\n':
+                ++y,x=0;
+                break;
+
+            default:
+            VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+            ++x;
+        }
+        
+        if(x >= 80)
+        {
+            ++y;
+            x=0;
+        }
+        
+        if(y >= 25)
+        {
+            for(y=0;y<25;++y)
+                for(x=0;x<80;++x)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+                    
+            x=0;
+            y=0;
+        }
+    }
+    
+}
+
+void printfHex(uint8_t key)
+{
+    char* msg = "0x00";
+    char* hex = "0123456789ABCDEF";
+    msg[0] = hex[(key >> 4) & 0x0F];
+    msg[1] = hex[key & 0x0F];
+    printf(msg); 
+}
+
+class PrintfKeyboardEventHandler : public KeyboardEventHandler
+{
+public:
+    void OnKeyDown(char c)
+    {
+        char* str = " ";
+        str[0]=c;
+        printf(str);
+    }
+};
+
+class MouseToConsole : public MouseEventHandler
+{
+    int8_t x,y;
+public:
+    MouseToConsole()
+    {
+        uint16_t* VideoMemory = (uint16_t*)0xb8000;
+        x=40;
+        y=12;
+        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4) 
+                            | ((VideoMemory[80*y+x] & 0x0F00) << 4) 
+                            | ((VideoMemory[80*y+x] & 0x00FF)); 
+    }
+    void OnMouseMovement(int xoffset, int yoffset)
+    {
+        static volatile uint16_t* VideoMemory = (uint16_t*)0xb8000; 
+        
+        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4) 
+                            | ((VideoMemory[80*y+x] & 0x0F00) << 4) 
+                            | ((VideoMemory[80*y+x] & 0x00FF)); 
+        
+        x += xoffset;
+        if(x < 0) x = 0;
+        if(x >=80) x=79;
+        
+        y += yoffset;
+        if(y < 0) y=0;
+        if(y>=25) y=24;
+        
+        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4) 
+                            | ((VideoMemory[80*y+x] & 0x0F00) << 4) 
+                            | ((VideoMemory[80*y+x] & 0x00FF)); 
+    }
+};
+
+typedef void (*constructor)();
+extern "C" constructor *start_ctors;
+extern "C" constructor *end_ctors;
+
+extern "C" void callConstructors()
+{
+    for(constructor *i= start_ctors; i!=end_ctors; ++i)
+        (*i)();
+}
+
+extern "C" void kernelMain(void* multiboot_structure, uint32_t /*multiboot_magic*/)
+{
+    bool open=1;
+    printf("Successfully booted BlockOS!\n\n");
+    
+    GlobalDescriptorTable gdt; 
+    InterruptManager interrupts(0x20, &gdt);
+    
+    printf("Initializing drivers...\n");
+    
+    DriverManager drvManager;
+    
+        PrintfKeyboardEventHandler kbhandler;
+        KeyboardDriver keyboard(&interrupts, &kbhandler);
+        drvManager.AddDriver(&keyboard);
+
+        MouseToConsole mousehandler;
+        MouseDriver mouse(&interrupts, &mousehandler);
+        drvManager.AddDriver(&mouse);
+        
+		PeripherialComponentInterconnectController PCIcontroller;
+		PCIcontroller.SelectDrivers(&drvManager, &interrupts);
+		
+        drvManager.ActivateAll();
+    
+    interrupts.Activate();
+    printf("All set!\n");
+    while(open);
+}
